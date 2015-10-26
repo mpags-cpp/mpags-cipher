0a3fb53e0578904880867f59724ade5dd93d4ddb
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-10-26 13:37:58 +0000

    Implement mpags-cipher Exercise 2
    Add usage of variables examples, excluding those that fail to compile.

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -4,9 +4,31 @@
 
 //! Main function of the mpags-cipher program
 int main(int /*argc*/, char* /*argv*/[]) {
+  // Integer variable
+  int answer {1};
+  answer = 42;
+  std::cout << "The answer is " << answer << "\n";
+
+  // double with const
+  const double pi {3.14};
+  std::cout << "PI is " << pi << " to 2dp\n";
+
+  // Product...
+  std::cout << "answer * pi = " << answer*pi << "\n";
+
+  // Division
+  std::cout << "pi / answer = " << pi / answer << "\n";
+
+  int rewsna {24};
+  std::cout << "answer / rewsna = " << answer / rewsna << "\n";
+
   // Hello world for C++ using strings
   std::string msg {"Hello World!"};
   std::cout << msg << "\n";
+
+  auto singleChar = msg[4];
+  std::cout << "5th char of message = " << singleChar << "\n";
+
 
   // No requirement to return from main, but we do so for clarity and
   // consistency with other functions.
