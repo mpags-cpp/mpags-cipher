17aaf7fc3a66479d5cfdbdd48fae664dcd98d2e0
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-10-26 13:30:32 +0000

    Implement 'hello world' using strings

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,10 +1,12 @@
-// For std::cout
+// Standard Library includes
 #include <iostream>
+#include <string>
 
 //! Main function of the mpags-cipher program
 int main(int /*argc*/, char* /*argv*/[]) {
-  // Hello world for C++!
-  std::cout << "Hello World!\n";
+  // Hello world for C++ using strings
+  std::string msg {"Hello World!"};
+  std::cout << msg << "\n";
 
   // No requirement to return from main, but we do so for clarity and
   // consistency with other functions.
