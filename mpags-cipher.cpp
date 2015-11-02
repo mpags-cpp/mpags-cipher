43c1c6b9163f9a816204f9dbd6fc416cea1731f1
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-11-02 13:50:39 +0000

    Provide function declaration for transformChar
    Declare transformChar function before main(), move implementation of
    transform char to after main().

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -10,51 +10,8 @@
   !param a character to be transformed
   !return the transformed string
 */
-std::string transformChar(const char c) {
-  std::string inputText {""};
+std::string transformChar(const char c);
 
-  // Uppercase alphabetic characters
-  if (std::isalpha(c)) {
-    inputText += std::toupper(c);
-  }
-
-  // Transliterate digits to English words
-  switch (c) {
-    case '0':
-      inputText += "ZERO";
-      break;
-    case '1':
-      inputText += "ONE";
-      break;
-    case '2':
-      inputText += "TWO";
-      break;
-    case '3':
-      inputText += "THREE";
-      break;
-    case '4':
-      inputText += "FOUR";
-      break;
-    case '5':
-      inputText += "FIVE";
-      break;
-    case '6':
-      inputText += "SIX";
-      break;
-    case '7':
-      inputText += "SEVEN";
-      break;
-    case '8':
-      inputText += "EIGHT";
-      break;
-    case '9':
-      inputText += "NINE";
-      break;
-  }
-
-  // Discard everything else as our ciphers cannot work with them
-  return inputText;
-}
 
 //! Main function of the mpags-cipher program
 int main(int argc, char* argv[]) {
@@ -166,4 +123,51 @@ int main(int argc, char* argv[]) {
   // No requirement to return from main, but we do so for clarity and
   // consistency with other functions.
   return 0;
+}
+
+
+std::string transformChar(const char c) {
+  std::string inputText {""};
+
+  // Uppercase alphabetic characters
+  if (std::isalpha(c)) {
+    inputText += std::toupper(c);
+  }
+
+  // Transliterate digits to English words
+  switch (c) {
+    case '0':
+      inputText += "ZERO";
+      break;
+    case '1':
+      inputText += "ONE";
+      break;
+    case '2':
+      inputText += "TWO";
+      break;
+    case '3':
+      inputText += "THREE";
+      break;
+    case '4':
+      inputText += "FOUR";
+      break;
+    case '5':
+      inputText += "FIVE";
+      break;
+    case '6':
+      inputText += "SIX";
+      break;
+    case '7':
+      inputText += "SEVEN";
+      break;
+    case '8':
+      inputText += "EIGHT";
+      break;
+    case '9':
+      inputText += "NINE";
+      break;
+  }
+
+  // Discard everything else as our ciphers cannot work with them
+  return inputText;
 }
